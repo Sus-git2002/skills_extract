c183d3440866a688bbfff8f333e1d9fd6f3bdaf5
Sus-git2002 <tvsusmitha@gmail.com>
2025-12-24 23:42:03 +0530

    Revert "Initial commit: skill extraction project setup"
    This reverts commit d44f8cee6af49ea58dcfe88b5a609ecdb24dbb50.

--- skills_extract/src/config/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Configuration management for the skills extraction pipeline."""
-
-from .config_loader import ConfigLoader, ConfigurationError
-
-__all__ = ["ConfigLoader", "ConfigurationError"]
--- skills_extract/src/visualization/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Visualization and analytics output modules."""
-
-from .analytics_generator import AnalyticsGenerator
-
-__all__ = ["AnalyticsGenerator"]
